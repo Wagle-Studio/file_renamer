1dcc12c440d4068429c3e836022314dad5aa3a6c
Kew <wolff.kevin@protonmail.com>
2025-02-24 22:08:01 +0100

    Extensions - install controlsfx and update main view GUI

--- src/main/java/com/filemanager/gui/views/MainView.java
+++ src/main/java/com/filemanager/gui/views/MainView.java
@@ -1,12 +1,17 @@
 package com.filemanager.gui.views;
 
+import org.controlsfx.control.CheckComboBox;
+
 import com.filemanager.gui.interactors.MainInteractor;
 import com.filemanager.gui.models.StrategyChoice;
+import com.filemanager.services.renaming.enums.FileExtension;
 
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Window;
 
 public final class MainView extends BaseView {
@@ -14,19 +19,19 @@ public final class MainView extends BaseView {
     public static final String TITLE = "FileManager";
     public static final String PATH = "main.fxml";
     private final MainInteractor interactor;
-    private final StrategyChoice placeholder = new StrategyChoice("Then, select a Strategy", null);
+    private final StrategyChoice strategyPlaceholder = new StrategyChoice("Select a treatment for you files", null);
     private boolean isFolderSelected = false;
 
     @FXML
-    private Button buttonSelectFolder;
+    private VBox dropBox;
     @FXML
     private Label labelSelectedFolderPath;
     @FXML
     private ChoiceBox<StrategyChoice> choiceBoxSelectStrategy;
     @FXML
-    private Label labelTaskStrategy;
-    @FXML
     private Button buttonStartAnalyse;
+    @FXML
+    private CheckComboBox<FileExtension> comboBoxSelectedExtensions;
 
     public MainView(MainInteractor interactor) {
         super(PATH, TITLE);
@@ -36,15 +41,17 @@ public final class MainView extends BaseView {
     @Override
     public void build() {
         this.initializeStrategyChoiceBox();
-        buttonSelectFolder.setOnAction(event -> this.handleFileSearch());
+        this.initializeExtensionComboBox();
+        dropBox.setOnMouseClicked(event -> this.handleFileSearch());
         buttonStartAnalyse.setOnAction(event -> this.handleStartAnalyse());
     }
 
     private void handleFileSearch() {
-        Window window = buttonSelectFolder.getScene().getWindow();
+        Window window = dropBox.getScene().getWindow();
         this.interactor.handleFileSearch(window);
 
         String selectedFolder = this.interactor.getSelectedFolder();
+
         if (selectedFolder != null && !selectedFolder.isEmpty()) {
             this.isFolderSelected = true;
             labelSelectedFolderPath.setVisible(true);
@@ -58,26 +65,34 @@ public final class MainView extends BaseView {
     }
 
     private void initializeStrategyChoiceBox() {
-        this.choiceBoxSelectStrategy.getItems().add(this.placeholder);
-        this.choiceBoxSelectStrategy.getItems().addAll(this.interactor.getStrategyChoices());
-        this.choiceBoxSelectStrategy.getSelectionModel().select(this.placeholder);
-        buttonStartAnalyse.setDisable(true);
+        choiceBoxSelectStrategy.getItems().add(this.strategyPlaceholder);
+        choiceBoxSelectStrategy.getItems().addAll(this.interactor.getStrategyChoices());
+        choiceBoxSelectStrategy.getSelectionModel().select(this.strategyPlaceholder);
 
         choiceBoxSelectStrategy.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null && !newValue.equals(this.placeholder)) {
+            if (!newValue.equals(this.strategyPlaceholder)) {
                 this.interactor.handleStrategyChoice(newValue);
-                labelTaskStrategy.setVisible(true);
-                labelTaskStrategy.setText(this.interactor.getSelectedStrategy());
             } else {
-                labelTaskStrategy.setVisible(false);
+                this.interactor.resetStrategyChoice();
             }
-
             updateStartButtonState();
         });
     }
 
+    private void initializeExtensionComboBox() {
+        comboBoxSelectedExtensions.getItems().addAll(FileExtension.values());
+
+        comboBoxSelectedExtensions.getCheckModel().getCheckedItems().addListener((ListChangeListener<FileExtension>) change -> {
+            while (change.next()) {
+                if (change.wasAdded() || change.wasRemoved()) {
+                    System.out.println("Extensions sélectionnées : " + comboBoxSelectedExtensions.getCheckModel().getCheckedItems());
+                }
+            }
+        });
+    }
+
     private void updateStartButtonState() {
-        boolean isStrategySelected = !choiceBoxSelectStrategy.getSelectionModel().getSelectedItem().equals(this.placeholder);
+        boolean isStrategySelected = !choiceBoxSelectStrategy.getSelectionModel().getSelectedItem().equals(this.strategyPlaceholder);
         buttonStartAnalyse.setDisable(!(this.isFolderSelected && isStrategySelected));
     }
 
